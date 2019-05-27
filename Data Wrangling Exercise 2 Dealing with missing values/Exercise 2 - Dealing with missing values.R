7f459da9a31dabf87bbf1976ea8cd0d72bfb83af
jpark <31976367+youngnosa@users.noreply.github.com>
2019-05-26 22:42:14 -0500

    Add files via upload

---
+++ Data Wrangling Exercise 2 Dealing with missing values/Exercise 2 - Dealing with missing values.R
@@ -0,0 +1,59 @@
+#0: Load the data in RStudio
+#Save the data set as a CSV file called titanic_original.csv and load it in RStudio into a data frame.
+
+library(dplyr)
+library(tidyr)
+titanic_original_csv <- read_csv("titanic_original.csv.csv")
+df <- titanic_original_csv
+summary(df)
+df = df[1:1309, ] ## removing empty line 13010
+
+#1: Port of embarkation
+#The embarked column has some missing values, which are known to correspond to passengers 
+#who actually embarked at Southampton. Find the missing values and replace them with S. 
+#(Caution: Sometimes a missing value might be read into R as a blank or empty string.)
+
+df$embarked[is.na(df$embarked)] <- "S" ## confirmed changes on table
+
+#2: Age
+#You'll notice that a lot of the values in the Age column are missing. While there are many ways to fill 
+#these missing values, using the mean or median of the rest of the values is quite common in such cases.
+#Calculate the mean of the Age column and use that value to populate the missing values
+#Think about other ways you could have populated the missing values in the age column. 
+#Why would you pick any of those over the mean (or not)?
+
+mean(df$age, na.rm = TRUE) #29.88113
+df$age[is.na(df$age)] <- mean(df$age, na.rm = TRUE)   
+anyNA(df$age) #FALSE, no more missing values
+
+#3: Lifeboat
+#You're interested in looking at the distribution of passengers in different 
+#lifeboats, but as we know, many passengers did not make it to a boat :-( 
+#This means that there are a lot of missing values in the boat column. Fill these empty 
+#slots with a dummy value e.g. the string 'None' or 'NA'
+
+anyNA(df$boat) #True, yes there are missing values
+df$boat[is.na(df$boat)] <- "None"
+anyNA(df$boat) #False, no more missing values
+
+#4: Cabin
+#You notice that many passengers don't have a cabin number associated with them.
+#Does it make sense to fill missing cabin numbers with a value?
+#What does a missing value here mean?
+#You have a hunch that the fact that the cabin number is missing might be a useful indicator of 
+#survival. Create a new column has_cabin_number which has 1 if there is a cabin number, and 0 otherwise.
+
+df <- mutate(df, has_cabin_number = ifelse(cabin == "NA", 0, 1)) 
+#ifelse(logical condition, execute if true, excute if false)
+df$has_cabin_number[is.na(df$has_cabin_number)] <- "0"
+
+
+#5: Submit the project on Github
+#Include your code, the original data as a CSV file titanic_original.csv, and the cleaned up data as a 
+#CSV file called titanic_clean.csv.
+
+write.csv(df, "refine_clean.csv")
+
+
+
+
